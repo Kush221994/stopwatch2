17460e5eb5b297e67d74c87623378a631273ebb8
Kushagra agarwal <kushagraagarwal22nov@gmail.com>
2022-06-01 16:59:07 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [mseconds, setMseconds] = useState(0);
+  const [stop, setStop] = useState(true);
+
+  const onstart = () => {
+    setStop(false);
+    // setMseconds(mseconds + 1);
+  };
+
+  const onstop = () => {
+    setStop(true);
+  };
+
+  const onreset = () => {
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+    setMseconds(0);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (!stop) {
+      interval = setInterval(() => {
+        if (minutes > 59) {
+          setHours(hours + 1);
+          setMinutes(0);
+          clearInterval(interval);
+        }
+        if (seconds > 59) {
+          setMinutes(minutes + 1);
+          setSeconds(0);
+          clearInterval(interval);
+        }
+        if (mseconds > 999) {
+          setSeconds(seconds + 1);
+          setMseconds(0);
+          clearInterval(interval);
+        }
+        if (mseconds <= 999) {
+          setMseconds(mseconds + 1);
+        }
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>
+          {" "}
+          {hours}Hr : {minutes}Min : {seconds}Sec : {mseconds}
+        </h1>
+        <button onClick={onstart}>Start</button>
+        <button onClick={onstop}>Stop</button>
+        <button onClick={onreset}>Reset</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
